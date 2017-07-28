9e2e6b26c092b264d1841d8b9c6306745a04a8fa
Andiry Xu <t-jianxu@microsoft.com>
2017-07-28 12:01:04 -0700

    Add channel checking to ocssd_unit

--- ocssd_server.h
+++ ocssd_server.h
@@ -40,14 +40,19 @@ public:
 
 class ocssd_channel {
 public:
-	int channel_id_;
-	int num_blocks_;
-	int used_count_;
 
-	ocssd_channel(int channel_id, int num_blocks)
+	ocssd_channel(size_t channel_id, size_t num_luns, size_t num_blocks)
 		:channel_id_(channel_id),
+		num_luns_(num_luns),
 		num_blocks_(num_blocks),
 		used_count_(0) {}
+
+private:
+
+	size_t channel_id_;
+	size_t num_luns_;
+	size_t num_blocks_;
+	size_t used_count_;
 };
 
 class ocssd_unit {
@@ -78,18 +83,82 @@ public:
 private:
 
 	int initialize_dev();
+	int channel_ok(size_t channel_id);
+	int assign_to_shared(ocssd_channel *channel);
 
 	std::string name_;
 	struct nvm_dev *dev_;
 	const struct nvm_geo *geo_;
+	int channel_count_ = 0;
 	std::vector<ocssd_channel *> shared_channels_;
 	std::vector<ocssd_channel *> exclusive_channels_;
 };
 
+int ocssd_unit::channel_ok(size_t channel_id)
+{
+	std::vector<struct nvm_addr> addrs;
+	struct nvm_addr addr;
+	struct nvm_vblk *blk;
+	int size = 4096 * 8;
+	void *buf;
+	ssize_t res;
+	int ret;
+
+	addr.ppa = 0;
+	addr.g.ch = channel_id;
+	addr.g.lun = 0;
+
+	addrs.push_back(addr);
+
+	blk = nvm_vblk_alloc(dev_, addrs.data(), addrs.size());
+
+	buf = nvm_buf_alloc(geo_, size);
+	if (!buf) {
+		printf("nvm_buf_alloc failed\n");
+		return -ENOMEM;
+	}
+
+	res = nvm_vblk_read(blk, buf, size);
+
+	ret = res == size ? 1 : 0;
+
+	free(buf);
+	return ret;
+}
+
+int ocssd_unit::assign_to_shared(ocssd_channel *channel)
+{
+	if (shared_channels_.size() < 4)
+		return 1;
+
+	return 0;
+}
+
 int ocssd_unit::initialize_dev()
 {
 	geo_ = nvm_dev_get_geo(dev_);
-	printf("geo: %lu channels, %lu LUNs\n", geo_->nchannels, geo_->nluns);
+	printf("geo: %lu channels, %lu LUNs, %lu blocks\n",
+		geo_->nchannels, geo_->nluns, geo_->nblocks);
+
+	for (size_t channel_id = 0; channel_id < geo_->nchannels; channel_id++) {
+		if (channel_ok(channel_id)) {
+			ocssd_channel *channel = new ocssd_channel(channel_id,
+							geo_->nluns,
+							geo_->nblocks);
+
+			if (assign_to_shared(channel))
+				shared_channels_.push_back(channel);
+			else
+				exclusive_channels_.push_back(channel);
+
+			channel_count_++;
+		}
+	}
+
+	std::cout << "Get " << channel_count_ << " channels for " << name_ << std::endl;
+	std::cout << "Share " << shared_channels_.size() << " channels, "
+		  << "exclusive " << exclusive_channels_.size() << " channels" << std::endl;
+
 	return 0;
 }
 
