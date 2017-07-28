ff56a69adba6bb05ce73524b58f96522207dc02b
Andiry Xu <t-jianxu@microsoft.com>
2017-07-27 17:50:21 -0700

    Update ocssd_server.h

--- ocssd_server.h
+++ ocssd_server.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <mutex>
 
 #define OCSSD_MAGIC 0xDEADBEEF
 
@@ -56,6 +57,8 @@ public:
 		dev_ = nvm_dev_open(name.c_str());
 		if (!dev_)
 			throw std::runtime_error("Error: open dev failed\n");
+
+		initialize_dev();
 	}
 
 	~ocssd_unit() {
@@ -68,19 +71,48 @@ public:
 		nvm_dev_close(dev_);
 	}
 
+	std::string get_name() {
+		return name_;
+	}
+
 private:
+
+	int initialize_dev();
+
 	std::string name_;
 	struct nvm_dev *dev_;
 	std::vector<ocssd_channel *> shared_channels_;
 	std::vector<ocssd_channel *> exclusive_channels_;
 };
 
+int ocssd_unit::initialize_dev()
+{
+	return 0;
+}
+
 class ocssd_manager {
 public:
-	std::unordered_map<int, ocssd_unit *> ocssds_;
+
+	int add_ocssd(std::string name);
 
 	~ocssd_manager() {
 		for (unsigned int i = 0; i < ocssds_.size(); i++)
 			delete ocssds_[i];
 	}
+
+private:
+
+	std::mutex mutex_;
+	std::unordered_map<int, ocssd_unit *> ocssds_;
+	int count_ = 0;
 };
+
+int ocssd_manager::add_ocssd(std::string name)
+{
+	ocssd_unit *unit = new ocssd_unit(name);
+	mutex_.lock();
+	ocssds_[count_] = unit;
+	count_++;
+	mutex_.unlock();
+	return 0;
+}
