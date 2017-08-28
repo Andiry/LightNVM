e386d6689a17ac74790f0a45031e01036ad617ca
Andiry Xu <t-jianxu@microsoft.com>
2017-08-28 13:29:14 -0700

    epoll for ocssd server

--- ocssd_server.cc
+++ ocssd_server.cc
@@ -8,11 +8,14 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/epoll.h>
 #include <boost/filesystem.hpp>
 
 #include "azure_config.h"
 #include "ocssd_server.h"
 
+#define MAX_FD 65536
+#define MAX_EVENT_NUMBER 10000
 #define BUFFER_SIZE 4096
 
 ocssd_manager *manager;
@@ -22,6 +25,39 @@ volatile sig_atomic_t stop;
 void interrupt(int signum) {
 	printf("%s\n", __func__);
 	stop = 1;
+}
+
+static int setnonblocking(int fd)
+{
+	int old_option = fcntl(fd, F_GETFL);
+	int new_option = old_option | O_NONBLOCK;
+	fcntl(fd, F_SETFL, new_option);
+	return old_option;
+}
+
+static void addfd(int epollfd, int fd, bool one_shot)
+{
+	epoll_event event;
+	event.data.fd = fd;
+	event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+	if (one_shot)
+		event.events |= EPOLLONESHOT;
+	epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+	setnonblocking(fd);
+}
+
+static void removefd(int epollfd, int fd)
+{
+	epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+	close(fd);
+}
+
+static void modfd(int epollfd, int fd, int ev)
+{
+	epoll_event event;
+	event.data.fd = fd;
+	event.events = ev | EPOLLONESHOT | EPOLLET | EPOLLRDHUP;
+	epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
 }
 
 static int publish_resource(ocssd_manager *manager)
@@ -105,19 +141,18 @@ int main(int argc, char **argv)
 	inet_pton(AF_INET, ip.c_str(), &address.sin_addr);
 	address.sin_port = htons(OCSSD_PORT);
 
-	int sock = socket(PF_INET, SOCK_STREAM, 0);
-	assert(sock >= 0);
+	int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+	assert(listenfd >= 0);
+	struct linger tmp = {1, 0};
+	setsockopt(listenfd, SOL_SOCKET, SO_LINGER, &tmp, sizeof(tmp));
 
-	ret = bind(sock, (struct sockaddr *)&address, sizeof(address));
+	ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
 	assert(ret != -1);
 
-	ret = listen(sock, 5);
+	ret = listen(listenfd, 5);
 	assert(ret != -1);
 
 	std::cout << "Listening on " << ip << ":" << OCSSD_PORT << "..." << std::endl;
-
-	struct sockaddr_in client;
-	socklen_t client_addrlen = sizeof(client);
 
 	struct sigaction action;
 	action.sa_handler = interrupt;
@@ -125,26 +160,53 @@ int main(int argc, char **argv)
 	action.sa_flags = 0;
 	sigaction(SIGINT, &action, NULL);
 
+	epoll_event events[MAX_EVENT_NUMBER];
+	int epollfd = epoll_create(5);
+	addfd(epollfd, listenfd, false);
+
 	while (!stop) {
-		int connfd = accept(sock, (struct sockaddr*)&client, &client_addrlen);
-		if (connfd < 0) {
-			printf("errno %d\n", errno);
-		} else {
-			char *buffer = new char[BUFFER_SIZE];
-			memset(buffer, 0, BUFFER_SIZE);
-			int received = 0;
-			received = recv(connfd, buffer, BUFFER_SIZE - 1, 0);
-			printf("Received %d\n", received);
-			process_request(connfd, buffer, received);
-			delete[] buffer;
-			close(connfd);
+		int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+		if (number < 0 && errno != EINTR) {
+			printf("epoll failure\n");
+			break;
+		}
+
+		for (int i = 0; i < number; i++) {
+			int sockfd = events[i].data.fd;
+			if (sockfd == listenfd) {
+				struct sockaddr_in client;
+				socklen_t client_addrlen = sizeof(client);
+
+				int connfd = accept(listenfd, (struct sockaddr*)&client,
+								&client_addrlen);
+
+				if (connfd < 0) {
+					printf("errno %d\n", errno);
+					continue;
+				} else {
+					char *buffer = new char[BUFFER_SIZE];
+					memset(buffer, 0, BUFFER_SIZE);
+					int received = 0;
+					received = recv(connfd, buffer, BUFFER_SIZE - 1, 0);
+					printf("Received %d\n", received);
+					process_request(connfd, buffer, received);
+					delete[] buffer;
+					close(connfd);
+				}
+			} else if (events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) {
+				// Close conn
+			} else if (events[i].events & EPOLLIN) {
+				printf("Sock %d: read request\n", sockfd);
+			} else if (events[i].events & EPOLLOUT) {
+				printf("Sock %d: write request\n", sockfd);
+			}
 		}
 	}
 
 	printf("Closing...\n");
 	manager->persist();
 	delete manager;
-	close(sock);
+	close(listenfd);
 
 	return 0;
 }
